8d0e9f38fcb8fcf598abc90295f68889a3864cc7
PKP323 <tiapuji.pkp@gmail.com>
2026-07-12 12:11:39 +0700

    Fix Selenium configuration and GitHub Actions

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -5,26 +5,35 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
     public static WebDriver driver;
 
     @Before
-    public void setup(){
+    public void setup() {
 
-       /*System.out.println("Setup dijalankan");*/
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
 
+        ChromeOptions options = new ChromeOptions();
+
+        // Hanya aktif di GitHub Actions
+        if (System.getenv("GITHUB_ACTIONS") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
     }
 
     @After
-    public void tearDown(){
-
-        driver.quit();
-
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
